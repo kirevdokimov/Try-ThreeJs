4e298f293c07f7326c691647c7af69d0ff6f93cd
Nobinator <nobinator.root@gmail.com>
2020-08-14 13:53:51 +0300

    ThreeJs + Webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path =  require('path')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
+    },
+    devServer: {
+        publicPath: '/public/',
+        compress: true,
+        port: 9000
+    }
+}
